78168dd08bbc740df5ea91f923721340d44e66d6
moeezamjad <amjadmoeez454@gmail.com>
2024-05-31 09:38:45 +0000

    variables

---
+++ 01-basics/1-variable.js
@@ -0,0 +1,8 @@
+const accountid=12345;
+console.log(accountid);
+let accountname="moeez";
+var accountpassword="1234";
+accountcity="germany";
+
+console.log(accountname);
+console.log([accountid,accountname,accountpassword,accountcity]);
